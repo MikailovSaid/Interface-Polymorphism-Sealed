a7f4c2608b6c2b63ec11a51aebba068f58688706
unknown <saidgm.code.edu.az>
2021-12-06 22:04:54 +0400

    Interface,Polymorphism,Sealed

---
+++ Interface/Interface/Man.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface
+{
+    public class Man : Human , LivingBeings
+    {
+        public string Run()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Speak()
+        {
+            Console.WriteLine("Salam!");
+        }
+    }
+}
---
+++ Interface/Interface/Swallow.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface
+{
+    internal class Swallow : Bird , LivingBeings
+    {
+        public string Run()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Sound()
+        {
+            Console.WriteLine("Cik cik");
+        }
+    }
+}
